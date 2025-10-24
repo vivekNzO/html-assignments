bc5f466e42be6909901b9dbc8e190928bfc2147d
vivekNzO <vivek@netzoptimize.com>
2025-10-24 19:02:47 +0530

    done till faqs

--- growthGuyClone/script.js
+++ growthGuyClone/script.js
@@ -1,16 +1,39 @@
-// $('#owl-carousel').owlCarousel({
-//     loop: true,
-//     margin: 30,
-//     dots: true,
-//     nav: true,
-//     items: 2,
-// })
-
-const owlCarousel = document.getElementById("owl-carousel")
-owlCarousel.owlCarousel({
+$('.owl-carousel').owlCarousel({
     loop:true,
-    margin:30,
-    dots:true,
-    nav:true,
-    items:2,
+    margin:10,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
 })
+
+var owl = $('.owl-carousel');
+
+$('.left-navigation').click(function() {
+    owl.trigger('prev.owl.carousel');
+});
+
+$('.right-navigation').click(function() {
+    owl.trigger('next.owl.carousel');
+});
+
+const questions = document.querySelectorAll(".question")
+console.log(questions)
+for(let i = 0;i<questions.length;i++){
+    questions[i].addEventListener("click",()=>{
+        let current = questions[i]
+        if(current.classList.contains("showAnswer")){
+            current.classList.remove("showAnswer")
+        }else{
+            current.classList.add("showAnswer")
+        }
+    })
+}
