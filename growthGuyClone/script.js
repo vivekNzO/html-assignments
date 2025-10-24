2fbf4426a5e938a371965ebe97b4c2cf875a2617
vivekNzO <vivek@netzoptimize.com>
2025-10-24 13:32:47 +0530

    blog-section added

---
+++ growthGuyClone/script.js
@@ -0,0 +1,16 @@
+// $('#owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 30,
+//     dots: true,
+//     nav: true,
+//     items: 2,
+// })
+
+const owlCarousel = document.getElementById("owl-carousel")
+owlCarousel.owlCarousel({
+    loop:true,
+    margin:30,
+    dots:true,
+    nav:true,
+    items:2,
+})
