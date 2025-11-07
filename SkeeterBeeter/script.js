bf632b571740e89360bd7226954d882e58fef507
vivekNzO <vivek@netzoptimize.com>
2025-11-07 15:18:37 +0530

    all pages done

--- SkeeterBeeter/script.js
+++ SkeeterBeeter/script.js
@@ -6,10 +6,32 @@
       const x = e.clientX / w - 0.5;
       const y = e.clientY / h - 0.5;
 
-      const rotateX = y * 40;  // how much to tilt vertically
-      const rotateY = -x * 40; // how much to tilt horizontally
-      const translateX = -x * 40; // lateral move
-      const translateY = -y * 40; // vertical move
+      const rotateX = y * 30;  // how much to tilt vertically
+      const rotateY = -x * 30; // how much to tilt horizontally
+      const translateX = -x * 30; // lateral move
+      const translateY = -y * 30; // vertical move
+
+
+      card.style.transform = `
+        rotateX(${rotateX}deg)
+        rotateY(${rotateY}deg)
+        translate(${translateX}px, ${translateY}px)
+        scale(1.05)
+      `;
+    });
+    })
+    const cards2 = document.querySelectorAll(".myAnimatedCard2");
+
+    cards2.forEach(card=>{
+      document.addEventListener("mousemove", (e) => {
+      const { innerWidth: w, innerHeight: h } = window;
+      const x = e.clientX / w - 0.5;
+      const y = e.clientY / h - 0.5;
+
+      const rotateX = y * 10;  // how much to tilt vertically
+      const rotateY = -x * 10; // how much to tilt horizontally
+      const translateX = -x * 10; // lateral move
+      const translateY = -y * 10; // vertical move
 
 
       card.style.transform = `
@@ -22,12 +44,12 @@
     })
 
 
-    const serviceDropdown = document.getElementById('nav-services')
+  const serviceDropdown = document.getElementById('nav-services')
 
-    serviceDropdown.addEventListener('click',()=>{
-      if(serviceDropdown.classList.contains('showDropDown')){
-        serviceDropdown.classList.remove('showDropDown')
-      }else{
-        serviceDropdown.classList.add('showDropDown');
-      }
-    })
+  serviceDropdown.addEventListener('click',()=>{
+    if(serviceDropdown.classList.contains('showDropDown')){
+      serviceDropdown.classList.remove('showDropDown')
+    }else{
+      serviceDropdown.classList.add('showDropDown');
+    }
+  })
